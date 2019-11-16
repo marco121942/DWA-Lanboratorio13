b6aab3eea4b5a3cc9c3236e33517a90c32d3b942
marco121942 <marco.sotomayor@tecsup.edu.pe>
2019-11-15 20:48:21 -0500

    first commit

---
+++ MVCAJAX/Controllers/StudentController.cs
@@ -0,0 +1,104 @@
+﻿//using Service;
+using System.Linq;
+using System.Web.Mvc;
+//using Domain;
+using MVCAJAX.Models;
+using System.Threading.Tasks;
+using System;
+
+namespace MVCAJAX.Controllers
+{    
+
+    public class StudentController : Controller
+    {
+        //private StudentService service = new StudentService();
+        Proxy.StudentProxy proxy = new Proxy.StudentProxy();
+
+        public ActionResult IndexRazor()
+        {
+
+            /*var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             ID = c.StundentID,
+                             StudentAddress = c.StudentAddress,
+                             StudentName = c.StudentName
+                         }).ToList();*/
+
+            var response = Task.Run(() => proxy.GetStudentsAsync());
+            return View(response.Result.listado);
+        }
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getStudent()
+        {
+
+            var response = Task.Run(() => proxy.GetStudentsAsync());
+            return Json(response.Result.listado, JsonRequestBehavior.AllowGet);
+            //return Json( service.Get(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Busqueda(string nombre)
+        {
+            var response = Task.Run(() => proxy.BuscarStudentsAsync(nombre));
+            return Json(response.Result.listado , JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public ActionResult EstudentDetail(int Id)
+        {
+
+            var response = Task.Run(() => proxy.GetByIdAsync(Id));
+            return Json(response.Result.objeto, JsonRequestBehavior.AllowGet);
+            //return Json(service.GetById(Id), JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public ActionResult CreateStudent(StudentModel std)
+        {
+            std.FechaCreacion = DateTime.Today;
+            //service.Insert(std);
+
+            var response = Task.Run(() => proxy.InsertAsync(std));
+            string message = response.Result.Mensaje;           
+           
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+
+        }
+
+
+        [HttpPost]
+        public ActionResult UpdateStudent(StudentModel std)
+        {
+            std.FechaModificacion = DateTime.Today;
+            //service.Update(std, Id);
+            var response = Task.Run(() => proxy.UpdateAsync(std));
+            string message = response.Result.Mensaje;
+
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+
+        }
+
+        [HttpPost]
+        public ActionResult DeleteStudent(int Id)
+        {
+            //service.Delete(Id);
+            var response = Task.Run(() => proxy.DeleteStudentAsync(Id));
+            string message = response.Result.Mensaje;
+
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+
+        }
+
+
+
+    }
+}
---
+++ MVCAJAX/Models/StudentModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAJAX.Models
+{
+    public class StudentModel
+    {
+
+        public int StundentID { get; set; }
+
+        public string Codigo { get; set; }
+
+        public string StudentName { get; set; }
+
+        public string StudentAddress { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? FechaCreacion { get; set; }
+
+        public DateTime? FechaModificacion { get; set; }
+
+        public bool? Activo { get; set; }
+    }
+}
